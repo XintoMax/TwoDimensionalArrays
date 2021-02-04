c05bd42e2088778be8ab44dc3094bdb546ce3757
Tyler Fowsantia <student.t.fowsantia@horizoncolumbus.org>
2021-02-04 09:45:01 -0500

    2d array processing fix

--- src/org/horizoncolumbus/hs/SumOfMatrix.java
+++ src/org/horizoncolumbus/hs/SumOfMatrix.java
@@ -6,14 +6,21 @@ public class SumOfMatrix {
     public static void main(String[] args) {
         int[][] matrix = {{0, 1, 1}, {9, 2, 4}, {9, 0, 4}, {1, 2, 5}};
         int sum = 0;
+        int rows = matrix.length;
+        int colm = matrix[0].length;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int x = 0; x < 3; x++) {
-                sum+= matrix[i][x];
+        for (int i = 0; i < rows; i++) {
+            int sumRows = 0;
+            for (int x = 0; x < colm; x++) {
+                sumRows = sumRows + matrix[i][x];
+            }
+            System.out.println("Sum of row " + (i + 1) + ": " + sumRows);
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int x = 0; x < colm; x++) {
                 System.out.print(matrix[i][x] + "\t");
             }
-            System.out.print("\n");
+            System.out.println("\n");
         }
-        System.out.println(sum);
     }
 }
